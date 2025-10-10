2be60e78272491f61c6eba4dd889793904bca86d
Fernando de Sande <fernando.desandegonzalez@plexus.es>
2025-10-10 13:46:34 +0100

    test(infrastructure): add test container and base test configuration
    - Created IntegrationTestBase as base to handle all TestContainer tests

--- src/test/java/org/fsg/assetmanager/AssetManagerApplicationTests.java
+++ src/test/java/org/fsg/assetmanager/AssetManagerApplicationTests.java
@@ -1,10 +1,11 @@
 package org.fsg.assetmanager;
 
+import org.fsg.assetmanager.infrastructure.IntegrationTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AssetManagerApplicationTests {
+class AssetManagerApplicationTests extends IntegrationTestBase {
 
     @Test
     void contextLoads() {
---
+++ src/test/java/org/fsg/assetmanager/infrastructure/IntegrationTestBase.java
@@ -0,0 +1,30 @@
+package org.fsg.assetmanager.infrastructure;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class IntegrationTestBase {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test")
+            .withReuse(true);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+    }
+}
